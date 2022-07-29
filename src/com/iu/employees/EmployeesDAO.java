a8d31c646ad3b23def3d6ce9ce55d4cca2edb71e
parkvsmin <sdfjkl658@naver.com>
2022-07-29 17:46:35 +0900

    11

--- src/com/iu/employees/EmployeesDAO.java
+++ src/com/iu/employees/EmployeesDAO.java
@@ -8,6 +8,37 @@ import com.iu.util.DBConnector;
 
 public class EmployeesDAO {
 	
+	public void getJoinTest()throws Exception {
+		//1.DB연결
+		Connection con = DBConnector.getConnection();
+		//2.SQL문작성
+		String sql = "SELECT E.LAST_NAME,E.SALARY, D.DEPARTMENT_NAME "
+				+ "FROM EMPLOYEES E "
+				+ "    INNER JOIN "
+				+ "    DEPARTMENTS D "
+				+ "    ON E.DEPARTMENT_ID = D.DEPARTMENT_ID "
+				+ "    WHERE E.EMPLOYEE_ID=?";
+		
+		//3.미리전송
+		PreparedStatement st = con.prepareStatement(sql);
+		Object employeesDTO;
+		//4.?가 있으면 값 세팅
+		st.setInt(1, employeesDTO.getEmployee_id());
+		//5.전송 후 결과 처리
+		ResultSet rs = st.executeQuery();
+		
+		if(rs.next() ) {
+			employeesDTO = new EmployeesDTO();
+			employeesDTO.setLast_name(rs.getString("LAST_NAME"));
+			employeesDTO.setSalary(rs.getInt("SALARY"));
+			DepartmentsDTO dt = new DepartmentsDTO();
+			dt.setDepartment_name(rs.getString("department_name"));
+			
+			
+		}
+		//6.연결해제
+	}
+	
 	public void getSalaryInfo() throws Exception {
 		//1. DB 연결
 		Connection con = DBConnector.getConnection();
