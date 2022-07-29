ecf556bf07c0b7292d35a0dcaacec0f475aebfc2
parkvsmin <sdfjkl658@naver.com>
2022-07-29 17:02:27 +0900

    11

---
+++ src/com/iu/employees/EmployeesDTO.java
@@ -0,0 +1,15 @@
+package com.iu.employees;
+
+public class EmployeesDTO {
+	
+	private Integer employeesDTO;
+
+	public Integer getEmployeesDTO() {
+		return employeesDTO;
+	}
+
+	public void setEmployeesDTO(Integer employeesDTO) {
+		this.employeesDTO = employeesDTO;
+	}
+
+}
